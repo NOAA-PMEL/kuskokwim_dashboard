07fadab2c243550c477e654fe81a5c3dfba02df1
shaunwbell <shaun.bell@noaa.gov>
2026-04-08 07:52:55 -0700

    break maps out of scripts

---
+++ scripts/run_mapgen.py
@@ -0,0 +1,62 @@
+# scripts/run_analysis.py
+import logging
+import folium
+import pandas as pd
+import urllib.request
+import datetime as dt
+# 👇 Imports now use the new package name
+from kuskokwim_dashboard import config, data_processing, mapping
+
+def main():
+    """
+    Main script to run the full data fetching, processing, and visualization workflow for the website, does not get ice/sst data.
+    """
+    # --- Setup ---
+    config.OUTPUT_DIR.mkdir(exist_ok=True)
+    config.IMAGE_DIR.mkdir(exist_ok=True)
+    
+    # --- Data Fetching and Processing ---
+    gdf_conc, gdf_pred = data_processing.fetch_ice_data()
+    gdf_conc_360 = data_processing.reproject_to_360(gdf_conc)
+    gdf_pred_360 = data_processing.reproject_to_360(gdf_pred)
+
+    # --- Output Dates ---
+    latest_conc_date = gdf_conc['idp_filedate'].max().strftime('%Y-%m-%d')
+    latest_pred_date = gdf_pred['idp_filedate'].max().strftime('%Y-%m-%d')
+    logging.info(f"Latest Ice Concentration Date: {latest_conc_date}")
+    logging.info(f"Latest Ice Prediction Date: {latest_pred_date}")
+    with open(f"{config.OUTPUT_DIR}/lastrun.txt", "w") as f:
+        print(f"Latest Ice Concentration Date: {latest_conc_date}", file=f)
+        print(f"Latest Ice Prediction Date: {latest_pred_date}", file=f)
+        print(f"Dashboard Updated: {dt.datetime.now().strftime('%Y-%m-%d %H:%M')}", file=f)
+
+    # --- Generate Maps ---
+    logging.info("Generating 'footprint.html' map...")
+    m1 = mapping.create_base_map(crs="EPSG3857", tiles="cartodb positron")
+    mapping.add_ice_concentration_layer(m1, gdf_conc_360.drop(columns=['idp_filedate','idp_ingestdate']))
+    mapping.add_ice_prediction_layer(m1, gdf_pred_360.drop(columns=['idp_filedate','idp_ingestdate']))
+    mapping.add_gebco_contours_layer(m1)
+    mapping.add_mooring_marker(m1)
+    mapping.add_adfg_grid_layer(m1)
+    folium.LayerControl().add_to(m1)
+    m1.save(config.OUTPUT_DIR / "footprint.html")
+
+    logging.info("Generating 'noaa_folium_map.html' map...")
+    m2 = mapping.create_base_map(crs="EPSG4326")
+    mapping.add_ice_concentration_layer(m2, gdf_conc_360.drop(columns=['idp_filedate','idp_ingestdate']))
+    mapping.add_ice_prediction_layer(m2, gdf_pred_360.drop(columns=['idp_filedate','idp_ingestdate']))
+    mapping.add_sst_wms_layer(m2)
+    mapping.add_adfg_grid_layer(m2, popup_on=False)
+    folium.LayerControl().add_to(m2)
+    m2.save(config.OUTPUT_DIR / "noaa_folium_map.html")
+    logging.info("Getting SST legend image from url.")
+    ## old erddap sst
+    # legend_url = mapping.get_sst_legend()
+    # urllib.request.urlretrieve(legend_url,'images/erddap_legend_sst.png')
+    # logging.info(f"URL for erddap legend: {legend_url}")
+
+
+    logging.info("Workflow complete.")
+
+if __name__ == "__main__":
+    main()
