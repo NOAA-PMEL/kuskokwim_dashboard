0a5b02f277e6b57199d18338938b200991a33dbd
shaunwbell <shaun.bell@noaa.gov>
2026-04-16 15:20:00 -0700

    ad medium sized plot

--- scripts/run_plotgen.py
+++ scripts/run_plotgen.py
@@ -52,6 +52,7 @@ def main():
     logging.info("Generating region timeseries...")
     # plotting.timeseries_plots(df, pdf)
     plotting.timeseries_plotly_plots(df, cdf, pdf,size='small',offseason=False)
+    plotting.timeseries_plotly_plots(df, cdf, pdf,size='medium',offseason=False)
     plotting.timeseries_plotly_plots(df, cdf, pdf,size='large',offseason=False)
 
 
